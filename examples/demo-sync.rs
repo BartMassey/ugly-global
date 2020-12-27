636f18aa2fe219832d1ddfd7a2b9d374919f2ed2
Bart Massey <bart.massey@gmail.com>
2020-12-27 15:57:03 -0800

    restructured examples

---
+++ examples/demo-sync.rs
@@ -0,0 +1,26 @@
+use ugly_global::*;
+
+global_vars! { sync,
+    GLOBAL_S: S;
+}
+
+struct S {
+    x: usize,
+    y: usize,
+}
+
+fn demo() {
+    fetch!(s = GLOBAL_S);
+    s.x += 1;
+    s.y += 2;
+    let result = s.x + s.y;
+    println!("{} {} {}", s.x, s.y, result);
+}
+
+fn main() {
+    init!(GLOBAL_S = S { x: 0, y: 0 });
+    demo();
+    demo();
+    let t = std::thread::spawn(|| demo());
+    t.join().unwrap();
+}
